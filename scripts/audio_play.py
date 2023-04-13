d020cc71e2ba9013e1f6a5c9ef1b7f11f2de90e7
jsbyysheng <jsbyysheng@gmail.com>
2023-04-13 17:05:59 +0800

    sync

---
+++ scripts/audio_play.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import rospy
+import sounddevice as sd
+import numpy as np
+import queue
+import sys
+import sounddevice as sd
+from ctl_msgs.msg import AudioData
+
+'''
+~output_device: use `python3 -m sounddevice` to get device list, numerical device ID or case-insensitive substrings is ok.
+~channels: 1
+~refresh_rate: 30
+~latency: 'high'
+~blocksize: 512
+~dtype: 'float32'
+~samplerate: 44100 48000 88200 96000 192000
+'''
+
+
+class audio_play:
+    def __init__(self):
+        self.initROS()
+        self.q = queue.Queue()
+        self.q_connects = queue.Queue()
+        self.q_channels = queue.Queue()
+        self.stream = sd.OutputStream(device=self.output_device,
+                                      samplerate=self.samplerate,
+                                      blocksize=self.blocksize,
+                                      dtype=self.dtype,
+                                      latency=self.latency,
+                                      channels=self.channels,
+                                      callback=self.audio_callback)
+        with self.stream:
+            rospy.spin()
+
+    def initROS(self):
+        rospy.init_node('audio_record', anonymous=True)
+        self.output_device = rospy.get_param("~output_device", default=None)
+        self.channels = rospy.get_param("~channels", default=1)
+        self.refresh_rate = rospy.get_param("~refresh_rate", default=30)
+        self.latency = rospy.get_param("~latency", default='high')
+        self.blocksize = rospy.get_param("~blocksize", default=512)
+        self.dtype = rospy.get_param("~dtype", default='float32')
+        self.samplerate = rospy.get_param("~samplerate", default=48000)
+        rospy.Subscriber('/audio_record_data', AudioData, self.AudioData_callback)
+
+    def audio_callback(self, outdata, frames, time, status):
+        if status:
+            rospy.logwarn(status)
+        try:
+            data = self.q.get_nowait()
+            connects = self.q_connects.get_nowait()
+            in_channels = self.q_channels.get_nowait()
+            data = data.reshape(self.blocksize, in_channels)
+            if len(connects) / 2 != len(connects) // 2:
+                raise Exception
+            for idx in range(0, len(connects) // 2):
+                if (connects[idx * 2] in range(0, self.channels)) and (connects[idx * 2 + 1] in range(0, self.channels)):
+                    outdata[:, connects[idx * 2 + 1]] = data[:, connects[idx * 2]]
+        except queue.Empty as e:
+            # rospy.logwarn('Buffer is empty: increase buffersize?')
+            outdata[:] = np.zeros_like(outdata)
+
+    def AudioData_callback(self, AudioData):
+        self.q.put(np.frombuffer(AudioData.data, dtype=self.dtype))
+        self.q_connects.put(np.frombuffer(AudioData.connects, dtype='uint32'))
+        self.q_channels.put(AudioData.channels)
+
+
+if __name__ == '__main__':
+    audio_play()
---
+++ scripts/audio_record.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+import rospy
+import sounddevice as sd
+import numpy as np
+import queue
+import sounddevice as sd
+from ctl_msgs.msg import AudioData, AudioDisplayData
+from std_msgs.msg import Float32
+from std_srvs.srv import SetBool, SetBoolResponse
+
+
+'''
+~input_device: use `python3 -m sounddevice` to get device list, numerical device ID or case-insensitive substrings is ok.
+~channels: 1
+~refresh_rate: 30
+~latency: 'high'
+~blocksize: 512
+~dtype: 'float32'
+~samplerate: 44100 48000 88200 96000 192000
+~downsample: 10 for display
+~connects: '0:0'
+'''
+
+
+class audio_record:
+    def __init__(self):
+        self.initROS()
+        self.initAudioDevice()
+        self.ctl_signal_old = not self.ctl_signal
+        with self.stream:
+            while not rospy.is_shutdown():
+                if self.ctl_signal != self.ctl_signal_old:
+                    self.ctl_signal_old = self.ctl_signal
+                    if self.ctl_signal:
+                        self.stream.start()
+                        rospy.loginfo('Audio Device {} Open'.format(self.input_device))
+                    else:
+                        self.stream.stop()
+                        rospy.loginfo('Audio Device {} Close'.format(self.input_device))
+                if self.ctl_signal_old:
+                    try:
+                        data = self.vis_q.get_nowait()
+                        (rows, cols) = data.shape
+                        seq_start = self.audiodispdata.header.seq
+                        stamp = rospy.Time.now()
+                        for c in range(0, cols):
+                            self.audiodispdata.header.seq = seq_start
+                            for r in range(0, rows):
+                                self.audiodispdata.header.seq = self.audiodispdata.header.seq + 1
+                                self.publish_disp(data[r, c], c, stamp, self.audiodispdata.header.seq)
+                    except queue.Empty:
+                        # rospy.logwarn('Visualization Queue is empty!')
+                        pass
+                self.vis_interval.sleep()
+
+    def initROS(self):
+        rospy.init_node('audio_record', anonymous=True)
+        self.input_device = rospy.get_param("~input_device", default=None)
+        self.channels = rospy.get_param("~channels", default=1)
+        self.refresh_rate = rospy.get_param("~refresh_rate", default=30)
+        self.latency = rospy.get_param("~latency", default='high')
+        self.blocksize = rospy.get_param("~blocksize", default=512)
+        self.dtype = rospy.get_param("~dtype", default='float32')
+        self.samplerate = rospy.get_param("~samplerate", default=48000)
+        self.downsample = rospy.get_param("~downsample", default=10)
+        self.connects = rospy.get_param("~connects", default='0:0')
+        self.pub_data_visualization = []
+        for ch in range(0, self.channels):
+            self.pub_data_visualization.append(rospy.Publisher('audio_record_data_visualization_ch' + str(ch), AudioDisplayData, queue_size=10))
+        self.pub_data = rospy.Publisher('audio_record_data', AudioData, queue_size=10)
+        self.audio_control_service = rospy.Service('audio_control_service', SetBool, self.audio_control_service_call)
+
+        self.ctl_signal = True
+
+        self.audiodata = AudioData()
+        self.connects = self.connects.replace(' ', '')
+        connects_tmp = []
+        for res_tmp in self.connects.split(','):
+            connects_tmp.append(res_tmp[0])
+            connects_tmp.append(res_tmp[2])
+        self.audiodata.channels = self.channels
+        self.audiodata.connects = np.array(connects_tmp, dtype='uint32').tobytes()
+        self.audiodata.header.stamp = rospy.Time.now()
+        self.audiodata.header.frame_id = ''
+        self.audiodata.header.seq = -1
+
+        self.audiodispdata = AudioDisplayData()
+        self.audiodispdata.value = 0.0
+        self.audiodispdata.header.stamp = self.audiodata.header.stamp
+        self.audiodispdata.header.frame_id = ''
+        self.audiodispdata.header.seq = -1
+
+        self.vis_interval = rospy.Rate(self.refresh_rate)
+
+    def initAudioDevice(self):
+        self.vis_q = queue.Queue()
+        self.stream = sd.InputStream(device=self.input_device,
+                                     samplerate=self.samplerate,
+                                     blocksize=self.blocksize,
+                                     dtype=self.dtype,
+                                     latency=self.latency,
+                                     channels=self.channels,
+                                     callback=self.audio_callback)
+
+    def audio_callback(self, indata, frames, time, status):
+        if status:
+            rospy.logerr(status)
+        data2send = bytes()
+        for ch_num in range(0, self.channels):
+            data2send = data2send + indata[:, ch_num].tobytes()
+        self.vis_q.put(indata[:: self.downsample, :])
+        self.publish(data2send)
+
+    def publish(self, bytes_data):
+        self.audiodata.header.stamp = rospy.Time.now()
+        self.audiodata.header.seq = self.audiodata.header.seq + 1
+        self.audiodata.data = bytes_data
+        self.pub_data.publish(self.audiodata)
+
+    def publish_disp(self, val, ch, stamp, seq):
+        self.audiodispdata.header.stamp = stamp
+        self.audiodispdata.header.seq = seq
+        self.audiodispdata.value = val
+        self.pub_data_visualization[ch].publish(self.audiodispdata)
+
+    def audio_control_service_call(self, req):
+        self.ctl_signal = req.data
+        return SetBoolResponse(self.ctl_signal, '0')
+
+
+if __name__ == '__main__':
+    audio_record()
